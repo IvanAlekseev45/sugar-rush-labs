c2893098e36908c4ae5e2828707c88727dcd27bf
IvanAlekseev45 <alekseeviva1992@gmail.com>
2026-05-06 16:26:01 +0000

    Deploying to gh-pages from @ IvanAlekseev45/sugar-rush-labs@2382cc008412a37e5adba329c536f835b1a18691 🚀

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as l}from"./assets/vendor-B1ZHW564.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=c(e);fetch(e.href,s)}})();const a={dessertList:document.querySelector(".dessert-list")},d=async()=>{const r="https://deserts-store.b.goit.study/api",{data:t}=await l.get(`${r}/desserts`,{params:{page:1,limit:8}});return t},u=async()=>{try{const{desserts:r}=await d(),t=r.map(({id:c,name:i,description:e,price:s,category:o,image:n})=>`
+        <li class="dessert-list__item">
+            <div class="dessert-list__wrapper">
+                <img class='dessert-list__image' src="${n}" alt="${i}" />
+                <p class="dessert-list__category">${o.name}</p>
+            
+                <p class="dessert-list__title">${i}</p>
+                <p class="dessert-list__description">${e}</p>    
+            </div>
+            <div class="dessert-list__wrapper__bottom">
+                <p class="dessert-list__price">${s} грн</p>
+                <button class="dessert-list__btn" type="button">
+                    <svg width="24" height="24" class="dessert-list__icon" aria-hidden="true" >
+                        <use href='/img/sprite.svg#icon-arrow_outward'></use>
+                    </svg>
+                </button>
+            </div>
+        </li>
+      `).join("");a.dessertList.insertAdjacentHTML("beforeend",t)}catch(r){console.log(r)}};u();
 //# sourceMappingURL=index.js.map

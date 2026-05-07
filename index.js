b6b6d33d96b22ac5a3ce904ce980aab3fa7b7e97
IvanAlekseev45 <alekseeviva1992@gmail.com>
2026-05-07 15:10:05 +0000

    Deploying to gh-pages from @ IvanAlekseev45/sugar-rush-labs@73ee9714b0f20931319cbad0eebc2b1a34de4bf0 🚀

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
-import{a as p,A as f,S as w,N as h,P as L}from"./assets/vendor-Cds40s8X.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const n of s)if(n.type==="childList")for(const d of n.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&a(d)}).observe(document,{childList:!0,subtree:!0});function o(s){const n={};return s.integrity&&(n.integrity=s.integrity),s.referrerPolicy&&(n.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?n.credentials="include":s.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function a(s){if(s.ep)return;s.ep=!0;const n=o(s);fetch(s.href,n)}})();const r={dessertList:document.querySelector(".dessert-list"),dessertCategory:document.querySelector(".dessert-category"),dessertLoadMoreBtn:document.querySelector(".dessert-load-more-btn"),dropdown:document.querySelector(".category-dropdown"),dropdownBtn:document.querySelector(".category-dropdown__btn"),dropdownText:document.querySelector(".category-dropdown__text")},v=async()=>{const e="https://deserts-store.b.goit.study/api",{data:t}=await p.get(`${e}/categories`);return t},B=async()=>{try{const e=await v(),t=`
+import{a as y,A as w,S as _,N as f,P as v}from"./assets/vendor-Cds40s8X.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))a(o);new MutationObserver(o=>{for(const d of o)if(d.type==="childList")for(const n of d.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function t(o){const d={};return o.integrity&&(d.integrity=o.integrity),o.referrerPolicy&&(d.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?d.credentials="include":o.crossOrigin==="anonymous"?d.credentials="omit":d.credentials="same-origin",d}function a(o){if(o.ep)return;o.ep=!0;const d=t(o);fetch(o.href,d)}})();const r={dessertList:document.querySelector(".dessert-list"),dessertCategory:document.querySelector(".dessert-category"),dessertLoadMoreBtn:document.querySelector(".dessert-load-more-btn"),dropdown:document.querySelector(".category-dropdown"),dropdownBtn:document.querySelector(".category-dropdown__btn"),dropdownText:document.querySelector(".category-dropdown__text"),dessertLoader:document.querySelector(".dessert-loader")},B=async()=>{const e="https://deserts-store.b.goit.study/api",{data:s}=await y.get(`${e}/categories`);return s},M=async()=>{try{const e=await B(),s=`
     <li class="dessert-category__item">
         <button class="dessert-category__btn active__btn" type="button" data-id="all">Всі десерти</button>
     </li>
-  `,o=e.map(({_id:a,name:s})=>`
+  `,t=e.map(({_id:a,name:o})=>`
         <li class="dessert-category__item">
-            <button class="dessert-category__btn" type="button" data-id="${a}">${s}</button>
+            <button class="dessert-category__btn" type="button" data-id="${a}">${o}</button>
         </li>
-      `).join("");r.dessertCategory.insertAdjacentHTML("beforeend",t+o)}catch(e){console.log(e)}},g=async(e=1,t="all")=>{const o=t==="all"?"https://deserts-store.b.goit.study/api/desserts":`https://deserts-store.b.goit.study/api/desserts?category=${t}`,{data:a}=await p.get(`${o}`,{params:{page:e,limit:8}});return a},y=e=>{const t=e.map(({_id:o,name:a,description:s,price:n,category:d,image:c})=>`
+      `).join("");r.dessertCategory.insertAdjacentHTML("beforeend",s+t)}catch(e){console.log(e)}},g=async(e=1,s="all")=>{const t=s==="all"?"https://deserts-store.b.goit.study/api/desserts":`https://deserts-store.b.goit.study/api/desserts?category=${s}`,{data:a}=await y.get(`${t}`,{params:{page:e,limit:8}});return a},m=e=>{const s=e.map(({_id:t,name:a,description:o,price:d,category:n,image:p})=>`
       <li class="dessert-list__item">
         <div class="dessert-list__wrapper">
-          <img class='dessert-list__image' src="${c}" alt="${a}" />
-          <p class="dessert-list__category">${d.name}</p>
+
+          <div class='loader-sizes-skeleton'>
+            <div class="loader dessert-loader">
+              <div class="cup">
+                <div class="cup-handle"></div>
+                <div class="smoke one"></div>
+                <div class="smoke two"></div>
+                <div class="smoke three"></div>
+              </div>
+              <div class="load">..........................</div>
+            </div>
+          </div>
+
+          <img class="dessert-list__image" src="${p}" alt="${a}" />
+          <p class="dessert-list__category">${n.name}</p>
 
           <p class="dessert-list__title">${a}</p>
-          <p class="dessert-list__description">${s}</p>
+          <p class="dessert-list__description">${o}</p>
         </div>
         <div class="dessert-list__wrapper__bottom">
-          <p class="dessert-list__price">${n} грн</p>
-          <button class="dessert-list__btn js-dessert-modal-open" type="button" data-id="${o}" >
-            <svg width="17" height="17" class="dessert-list__icon" aria-hidden="true" >
-              <use href='/img/sprite.svg#icon-arrow_outward'></use>
+          <p class="dessert-list__price">${d} грн</p>
+          <button class="dessert-list__btn js-dessert-modal-open" type="button" data-id="${t}">
+            <svg width="17" height="17" class="dessert-list__icon" aria-hidden="true">
+              <use href="/img/sprite.svg#icon-arrow_outward"></use>
             </svg>
           </button>
         </div>
       </li>
-      `).join("");r.dessertList.insertAdjacentHTML("beforeend",t)},M=async(e=1,t="all")=>{try{const{desserts:o}=await g(e,t);y(o),r.dessertLoadMoreBtn.classList.remove("dessert-button-hidden")}catch(o){console.log(o)}};let l=1,u="all";const b=e=>{const t=Math.ceil(e/8);l<t?(r.dessertLoadMoreBtn.classList.remove("dessert-button-hidden"),r.dessertLoadMoreBtn.disabled=!1):(r.dessertLoadMoreBtn.classList.add("dessert-button-hidden"),r.dessertLoadMoreBtn.disabled=!0)},$=async e=>{const t=e.target.closest(".dessert-category__btn");if(!t)return;l=1,u=t.dataset.id,document.querySelectorAll(".dessert-category__btn").forEach(a=>a.classList.remove("active__btn")),t.classList.add("active__btn"),r.dessertList.innerHTML="";const o=await g(l,u);y(o.desserts),b(o.totalItems)},S=async()=>{r.dessertLoadMoreBtn.classList.add("dessert-button-hidden"),r.dessertLoadMoreBtn.disabled=!0,l+=1;try{const e=await g(l,u);y(e.desserts),b(e.totalItems)}catch(e){console.log(e)}},m={handleCategoryFilter:$,handleLoadMoreDesserts:S};r.dropdownBtn.addEventListener("click",()=>{r.dropdown.classList.toggle("is-open")});r.dropdown.addEventListener("click",e=>{const t=e.target.closest(".dessert-category__btn");t&&(r.dropdownText.textContent=t.textContent,r.dropdown.classList.remove("is-open"))});document.addEventListener("click",e=>{r.dropdown.contains(e.target)||r.dropdown.classList.remove("is-open")});B();M();r.dessertCategory.addEventListener("click",m.handleCategoryFilter);r.dessertLoadMoreBtn.addEventListener("click",m.handleLoadMoreDesserts);new f(".accordion-container",{showMultiple:!0,duration:300});const _={bestsellersList:document.querySelector(".bestsellers-list"),loaderBestsellers:document.querySelector(".bestsellers-loader"),leftBtn:document.querySelector(".page-left-btn"),rightBtn:document.querySelector(".page-right-btn")},q=async()=>{const e="https://deserts-store.b.goit.study/api",{data:t}=await p.get(`${e}/desserts?type=popular`);return t};async function C(){const e=_.loaderBestsellers;e.style.display="block";try{const{desserts:t}=await q();E(t)}catch(t){console.error(t)}finally{e.style.display="none"}}document.addEventListener("DOMContentLoaded",C);let i=null;function E(e){const t=e.map(({image:o,category:a,description:s,name:n,price:d,_id:c})=>`<li class="bestsellers-list-item swiper-slide">
-          <img class="bestsellers-image" src="${o}" alt="${n}"/>
+      `).join("");r.dessertList.insertAdjacentHTML("beforeend",s),r.dessertList.querySelectorAll(".dessert-list__image").forEach(t=>{const a=t.previousElementSibling;if(t.complete){a.style.display="none",t.classList.add("img__is-loaded");return}t.addEventListener("load",()=>{a.style.display="none",t.classList.add("img__is-loaded")}),t.addEventListener("error",()=>{a.style.display="none",t.classList.add("img__is-loaded")})})},$=()=>{r.dessertLoader.style.display="block"},S=()=>{r.dessertLoader.style.display="none"},l={showLoader:$,hideLoader:S},q=async(e=1,s="all")=>{l.showLoader();try{const{desserts:t}=await g(e,s);m(t),l.hideLoader(),r.dessertLoadMoreBtn.classList.remove("dessert-button-hidden")}catch(t){console.log(t)}};let i=1,u="all";const b=e=>{const s=Math.ceil(e/8);i<s?(r.dessertLoadMoreBtn.classList.remove("dessert-button-hidden"),r.dessertLoadMoreBtn.disabled=!1):(r.dessertLoadMoreBtn.classList.add("dessert-button-hidden"),r.dessertLoadMoreBtn.disabled=!0)},E=async e=>{const s=e.target.closest(".dessert-category__btn");if(s){i=1,u=s.dataset.id,r.dessertList.innerHTML="",l.showLoader(),await new Promise(t=>requestAnimationFrame(t));try{const t=await g(i,u);m(t.desserts),b(t.totalItems)}catch(t){console.log(t)}finally{l.hideLoader()}}},k=async()=>{r.dessertLoadMoreBtn.classList.add("dessert-button-hidden"),r.dessertLoadMoreBtn.disabled=!0,l.showLoader(),i+=1;try{const e=await g(i,u);m(e.desserts),b(e.totalItems)}catch(e){console.log(e)}finally{l.hideLoader()}},L={handleCategoryFilter:E,handleLoadMoreDesserts:k};r.dropdownBtn.addEventListener("click",()=>{r.dropdown.classList.toggle("is-open")});r.dropdown.addEventListener("click",e=>{const s=e.target.closest(".dessert-category__btn");s&&(r.dropdownText.textContent=s.textContent,r.dropdown.classList.remove("is-open"))});document.addEventListener("click",e=>{r.dropdown.contains(e.target)||r.dropdown.classList.remove("is-open")});M();q();r.dessertCategory.addEventListener("click",L.handleCategoryFilter);r.dessertLoadMoreBtn.addEventListener("click",L.handleLoadMoreDesserts);new w(".accordion-container",{showMultiple:!0,duration:300});const h={bestsellersList:document.querySelector(".bestsellers-list"),loaderBestsellers:document.querySelector(".bestsellers-loader"),leftBtn:document.querySelector(".page-left-btn"),rightBtn:document.querySelector(".page-right-btn")},P=async()=>{const e="https://deserts-store.b.goit.study/api",{data:s}=await y.get(`${e}/desserts?type=popular`);return s};async function C(){const e=h.loaderBestsellers;e.style.display="block";try{const{desserts:s}=await P();j(s)}catch(s){console.error(s)}finally{e.style.display="none"}}document.addEventListener("DOMContentLoaded",C);let c=null;function j(e){const s=e.map(({image:t,category:a,description:o,name:d,price:n,_id:p})=>`<li class="bestsellers-list-item swiper-slide">
+          <img class="bestsellers-image" src="${t}" alt="${d}"/>
           <p class="bestsellers-category">${a.name}</p>
-          <h3 class="bestsellers-name">${n}</h3>
-          <p class="bestsellers-description">${s}</p>
+          <h3 class="bestsellers-name">${d}</h3>
+          <p class="bestsellers-description">${o}</p>
           <div class="bestsellers-wrapper">
-            <p class="bestsellers-price">${d} грн</p>
-            <button class="bestsellers-modal-btn js-dessert-modal-open" type="button" data-id="${c}">
+            <p class="bestsellers-price">${n} грн</p>
+            <button class="bestsellers-modal-btn js-dessert-modal-open" type="button" data-id="${p}">
               <svg width="24" height="24" aria-hidden="true">
                 <use href="/img/sprite.svg#icon-arrow_outward"></use>
               </svg>
             </button>
           </div>
-        </li>`).join("");_.bestsellersList.innerHTML=t,i&&(i.destroy(!0,!0),i=null),i=new w(".swiper-bestsellers",{modules:[h,L],pagination:{el:".swiper-pagination",dynamicBullets:!0},navigation:{prevEl:".page-left-btn",nextEl:".page-right-btn"},slidesPerView:1,spaceBetween:16,breakpoints:{768:{slidesPerView:2,spaceBetween:16},1280:{slidesPerView:3,spaceBetween:24}}})}
+        </li>`).join("");h.bestsellersList.innerHTML=s,c&&(c.destroy(!0,!0),c=null),c=new _(".swiper-bestsellers",{modules:[f,v],pagination:{el:".swiper-pagination",dynamicBullets:!0},navigation:{prevEl:".page-left-btn",nextEl:".page-right-btn"},slidesPerView:1,spaceBetween:16,breakpoints:{768:{slidesPerView:2,spaceBetween:16},1280:{slidesPerView:3,spaceBetween:24}}})}
 //# sourceMappingURL=index.js.map
